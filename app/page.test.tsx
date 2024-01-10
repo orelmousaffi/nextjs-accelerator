63b55fdae3db1e15779f8cfadcc5080f6d3206da
Or-el Mousaffi <orel.mousaffi@gmail.com>
2024-01-10 00:22:39 +0000

    feat: reorganize tests at the same level of the tested component

---
+++ app/page.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import { expect, test } from 'vitest';
+import Home from './page';
+
+test('Page', () => {
+  render(<Home />);
+  expect(screen.getByTestId('page-component')).toBeDefined();
+});
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import styles from './page.module.css';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main className={styles.main} data-testid="page-component">
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
